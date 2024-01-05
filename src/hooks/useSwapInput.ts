108fe3c167a556c55bd028a34801b12fb8c0cfd2
김진건 <87012465+jingeon27@users.noreply.github.com>
2024-01-05 10:28:39 +0900

    chore : ui 로직 분리

--- src/hooks/useSwapInput.ts
+++ src/hooks/useSwapInput.ts
@@ -1,1 +1,61 @@
-export const useSwapInput = () => {};
+import { useEffect, useMemo, useState } from 'react';
+import { useForm } from './useForm';
+import { useSuspenseQueries } from '@tanstack/react-query';
+import { getQuote } from 'apis';
+import { TokenIdType } from 'constants/tokenList';
+
+type stateType = {
+  first: TokenIdType;
+  second: TokenIdType;
+};
+type inputType = `${number}.${number}` | `${number}`;
+export type nowType = keyof stateType;
+type inputStateType = {
+  first: inputType;
+  second: inputType;
+};
+const initialInputState: inputStateType = {
+  first: '0.0',
+  second: '0.0',
+} as const;
+const initialState: stateType = {
+  first: 'axie-infinity',
+  second: 'aave',
+} as const;
+export const useSwapInput = () => {
+  const [state, changeState] = useForm<stateType>(initialState);
+  const [nowState, setNowState] = useState<nowType>('first');
+  const [inputState, changeInputState] =
+    useForm<inputStateType>(initialInputState);
+  const [firstQuote, secondQuote] = useSuspenseQueries({
+    queries: Object.keys(state).map((key) => {
+      const ids = state[key as keyof stateType];
+      return {
+        queryKey: [ids],
+        queryFn: () => getQuote(ids),
+      };
+    }),
+  });
+  const firstInputUsd = useMemo(
+    () => parseInt(inputState.first) * firstQuote.data,
+    [inputState.first, firstQuote.data],
+  );
+  const secondInputUsd = useMemo(
+    () => parseInt(inputState.second) * secondQuote.data,
+    [inputState.second, secondQuote.data],
+  );
+  const inputUsd = {
+    first: firstInputUsd,
+    second: secondInputUsd,
+  };
+  useEffect(() => {}, []);
+  return {
+    inputUsd,
+    setNowState,
+    state,
+    changeInputState,
+    changeState,
+    nowState,
+    inputState,
+  };
+};
--- src/pages/main.tsx
+++ src/pages/main.tsx
@@ -1,12 +1,9 @@
-import { useSuspenseQueries } from '@tanstack/react-query';
-import { getQuote } from 'apis';
 import { Input } from 'components/input';
 import { Modal } from 'components/modal';
 import { DropDown } from 'components/providers/DropDown';
 import { GlobalPortal } from 'components/providers/GlobalPotal';
-import { TokenIdType } from 'constants/tokenList';
-import { useForm } from 'hooks/useForm';
-import { Suspense, useEffect, useMemo, useState } from 'react';
+import { nowType, useSwapInput } from 'hooks/useSwapInput';
+import { Suspense } from 'react';
 
 export const MainPage = () => {
   return (
@@ -24,52 +21,18 @@ export const MainPage = () => {
     </>
   );
 };
-type stateType = {
-  first: TokenIdType;
-  second: TokenIdType;
-};
-type inputType = `${number}.${number}` | `${number}`;
-type nowType = keyof stateType;
-type inputStateType = {
-  first: inputType;
-  second: inputType;
-};
-const initialInputState: inputStateType = {
-  first: '0.0',
-  second: '0.0',
-} as const;
-const initialState: stateType = {
-  first: 'axie-infinity',
-  second: 'aave',
-} as const;
+
 const InputArray: nowType[] = ['first', 'second'];
 MainPage.SwapInputs = () => {
-  const [state, changeState] = useForm<stateType>(initialState);
-  const [nowState, setNowState] = useState<nowType>('first');
-  const [inputState, changeInputState] =
-    useForm<inputStateType>(initialInputState);
-  const [firstQuote, secondQuote] = useSuspenseQueries({
-    queries: Object.keys(state).map((key) => {
-      const ids = state[key as keyof stateType];
-      return {
-        queryKey: [ids],
-        queryFn: () => getQuote(ids),
-      };
-    }),
-  });
-  const firstInputUsd = useMemo(
-    () => parseInt(inputState.first) * firstQuote.data,
-    [inputState.first, firstQuote.data],
-  );
-  const secondInputUsd = useMemo(
-    () => parseInt(inputState.second) * secondQuote.data,
-    [inputState.second, secondQuote.data],
-  );
-  const inputUsd = {
-    first: firstInputUsd,
-    second: secondInputUsd,
-  };
-  useEffect(() => {}, []);
+  const {
+    inputUsd,
+    setNowState,
+    state,
+    changeInputState,
+    changeState,
+    nowState,
+    inputState,
+  } = useSwapInput();
   return (
     <>
       <DropDown.Provider>
